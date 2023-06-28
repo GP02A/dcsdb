9c84ca9c4accb5e22586e852339e31b842cc1f27
Bo <zealot37@gmail.com>
2023-06-28 15:17:55 +0800

    android added

---
+++ dcsdb-frontend/dist/assets/focus-visible-legacy-e7e63653.js
@@ -0,0 +1,5 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){
+/*!
+       * (C) Ionic http://ionicframework.com - MIT License
+       */
+const t="ion-focused",o=["Tab","ArrowDown","Space","Escape"," ","Shift","Enter","ArrowLeft","ArrowRight","ArrowUp","Home","End"];e("startFocusVisible",(e=>{let s=[],n=!0;const r=e?e.shadowRoot:document,c=e||document.body,i=e=>{s.forEach((e=>e.classList.remove(t))),e.forEach((e=>e.classList.add(t))),s=e},d=()=>{n=!1,i([])},a=e=>{n=o.includes(e.key),n||i([])},u=e=>{if(n&&void 0!==e.composedPath){const t=e.composedPath().filter((e=>!!e.classList&&e.classList.contains("ion-focusable")));i(t)}},v=()=>{r.activeElement===c&&i([])};return r.addEventListener("keydown",a),r.addEventListener("focusin",u),r.addEventListener("focusout",v),r.addEventListener("touchstart",d),r.addEventListener("mousedown",d),{destroy:()=>{r.removeEventListener("keydown",a),r.removeEventListener("focusin",u),r.removeEventListener("focusout",v),r.removeEventListener("touchstart",d),r.removeEventListener("mousedown",d)},setFocus:i}}))}}}));
