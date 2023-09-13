d28e968980fc5aeb9c80ca2d6697d23490dd4c81
Bo <zealot37@gmail.com>
2023-09-13 19:14:47 +0800

    pwa config

--- dcsdb-frontend/vite.config.ts
+++ dcsdb-frontend/vite.config.ts
@@ -1,12 +1,16 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //use base to setup pwa at subdirectory
+  // base: 'https://bologs.gp02a.com/pwa/dcsdb',
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   test: {
     globals: true,
